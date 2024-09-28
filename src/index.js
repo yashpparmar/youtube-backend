7fb0a3ce0a2ebb04a52649f69b450c207c03a1a0
Yash Parmar <87753564+yashpparmar@users.noreply.github.com>
2024-09-28 23:05:06 +0530

    added api error and response utils

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 // require("dotenv").config({ path: "/.env" }); // for commonJs
-// import dotenv from "dotenv";
-import "dotenv/config"; // for module
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-// dotenv.config({
-//   path: "/.env",
-// });
+dotenv.config({
+  path: "/.env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 8000;
+
+    app.listen(port, () => {
+      console.log(`⚙ Server is running at port: ${port}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+  });
 
 /*
 import express from "express";
